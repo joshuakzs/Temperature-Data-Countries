1e5abd9e22393a87b5490dc9d59c86e4ab6b0ee2
Joshua Koh Ze Shao <joshuakzs@u.nus.edu>
2024-02-27 00:57:24 +0800

    finished sg_extract_data.py file

--- Create_data_tables.sql
+++ Create_data_tables.sql
@@ -2,8 +2,8 @@ CREATE TABLE IF NOT EXISTS raw_sg_data (
         station_id SERIAL,
         station_name VARCHAR(50),
         temperature NUMERIC(7,5),
-        latitude NUMERIC(7,5),
-        longitude NUMERIC(7,5),
+        latitude NUMERIC(13,7),
+        longitude NUMERIC(13,7),
         date_queried DATE,
         time_queried TIME
 );
--- sg_extract_data.py
+++ sg_extract_data.py
@@ -1,19 +1,19 @@
-print("hello")
-
 import pandas as pd
 import requests
+import psycopg2
+import psycopg2.extras as extras
+
 r = requests.get("https://api.data.gov.sg/v1/environment/air-temperature")
 json_dict = r.json()
 if json_dict["api_info"]["status"] == "healthy":
-    #E.g. '2024-02-26T21:40:00+08:00'
     # Get temp data
     d = json_dict["items"][0]
-    timestamp = d["timestamp"]
+    timestamp = d["timestamp"] #E.g. '2024-02-26T21:40:00+08:00'
     readings = d["readings"]
     def reading_dict_pair_to_array_pair(array,timestamp):
         #e.g. [{'station_id': 'S117', 'value': 28.6}, {'station_id': 'S50', 'value': 27.7}] -> [["S117",28.6],["S50",27.7]]
         def helper(dict_pair):
-            return [dict_pair["station_id"],round(dict_pair["value"],2),timestamp]
+            return [int(dict_pair["station_id"][1:]),round(dict_pair["value"],2),timestamp]
         return list(map(helper,array))
     df_readings = pd.DataFrame(reading_dict_pair_to_array_pair(readings,timestamp), columns=["station_id","value","timestamp"])
     #Get station data
@@ -21,7 +21,7 @@ if json_dict["api_info"]["status"] == "healthy":
     def station_dict_to_array(array):
         #e.g. [{'id': 'S117', 'device_id': 'S117', 'name': 'Banyan Road', 'location': {'latitude': 1.256, 'longitude': 103.679}}, ...]
         def helper(dict):
-            device_id = dict["device_id"]
+            device_id = int(dict["device_id"][1:])
             station_name = dict["name"]
             latitude = dict["location"]["latitude"]
             longitude = dict["location"]["longitude"]
@@ -30,13 +30,40 @@ if json_dict["api_info"]["status"] == "healthy":
     df_stations = pd.DataFrame(station_dict_to_array(station_array),columns=["device_id","station_name","latitude","longitude"])
     #Combine dfs and do a bit of cleaning
     df_merged = df_readings.merge(df_stations,how = "inner",left_on = "station_id",right_on="device_id")
-    df_merged["timestamp"] = pd.to_datetime(df_merged["timestamp"],utc=True).dt.tz_convert(None)
-    df_merged["date"] = df_merged["timestamp"].dt.date
-    df_merged["time"] = df_merged["timestamp"].dt.time
-    df_final = df_merged[["station_id","station_name","value","latitude","longitude","date","time"]].rename(columns = {"value":"temperature"})
-    #Load data to PostgresSQL
+    df_merged["timestamp"] = pd.to_datetime(df_merged["timestamp"],utc=False).dt.tz_convert("Singapore")
+    df_merged["date_queried"] = df_merged["timestamp"].dt.date
+    df_merged["time_queried"] = df_merged["timestamp"].dt.time
+    df_final = df_merged[["station_id","station_name","value","latitude","longitude","date_queried","time_queried"]].rename(columns = {"value":"temperature"})
+    print(f"Number of new entries: {df_final.shape[0]}")
 
+    #Load data to PostgresSQL
+    conn = psycopg2.connect(host="localhost",
+                            dbname="tempdataetl",
+                            user="postgres",
+                            port=5432,
+                            password="TempProjectETL")
+    def execute_values(conn, df, table):
+
+        tuples = [tuple(x) for x in df.to_numpy()]
+
+        cols = ','.join(list(df.columns))
+        # SQL query to execute
+        query = "INSERT INTO %s(%s) VALUES %%s" % (table, cols)
+        cursor = conn.cursor()
+        try:
+            extras.execute_values(cursor, query, tuples)
+            conn.commit()
+        except (Exception, psycopg2.DatabaseError) as error:
+            print("Error: %s" % error)
+            conn.rollback()
+            cursor.close()
+            return 1
+        print("the dataframe is inserted")
+        cursor.close()
+
+    execute_values(conn, df_final, 'raw_sg_data')
 else:
     #Query failed
-    pass
+    print("API request brought in 'unhealthy' data.")
+    raise Exception
 print("hello")
